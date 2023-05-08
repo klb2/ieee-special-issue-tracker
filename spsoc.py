bf5da0834cf96227d83a29dfbb05f77de55e49cf
Karl Besser <kb1717@princeton.edu>
2023-05-08 12:03:49 -0400

    Update spsoc parser to new CFP site
    Update the signal processing parser to the new call for papers website
    that uses a new table to list all CFPs.

--- spsoc.py
+++ spsoc.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup
 
 from constants import COL_JOURNAL, COL_DEADLINE, COL_PUB_DATE, COL_TOPIC
 
-#URL = "https://signalprocessingsociety.org/publications-resources/special-issue-deadlines"
-URL = "https://signalprocessingsociety.org/publications-resources/special-issue-deadlines?tid=All&sort_by=field_date_value&sort_order=ASC&page={}"
+URL = "https://signalprocessingsociety.org/publications-resources/special-issue-deadlines"
+#URL = "https://signalprocessingsociety.org/publications-resources/special-issue-deadlines?tid=All&sort_by=field_date_value&sort_order=ASC&page={}"
 URL_SOC = "https://signalprocessingsociety.org"
 
 RE_POST_HEADER = r'(IEEE .+) Special (?:Issue|Series) on (.+)'
@@ -16,29 +16,20 @@ RE_DATE = r'.+: ((?:\d{1,2} )?\w+ (?:\d{1,2}, )?\d{4})'
 
 
 def get_all_cfp():
-    data = []
-    number = 0
-    while True:
-        try:
-            page_data = get_cfp_single_page(number)
-            data.append(page_data)
-        except ValueError:
-            break
-        number = number + 1
-    data = pd.concat(data, ignore_index=True)
-    return data
-
-def get_cfp_single_page(number: int):
-    url = URL.format(number)
-    resp = requests.get(url)
+    resp = requests.get(URL)
     soup = BeautifulSoup(resp.text, 'html.parser')
-    posts = soup.find_all("section", "post-content")
+    #posts = soup.find_all("section", "post-content")
+    _content = soup.find("div", {"class": "content"})
+    _table = soup.find("table")
+    _table = _table.find("tbody")
+    posts = _table.find_all("tr")
     rows = []
     for post in posts:
-        header = post.find("header").find("h2").text.strip()
-        #print(header)
+        _cells = post.find_all("td")
+        _info = _cells[0]
+        header = _info.find("h4").text.strip()
         journal, topic = re.match(RE_POST_HEADER, header).groups()
-        journal = f'<a href="{url}">{journal}</a>'
+        journal = f'<a href="{URL}">{journal}</a>'
         body = post.find("p")
         _date_strings = list(body.stripped_strings)
         due_date = re.match(RE_DATE, str(_date_strings[0]))[1]
