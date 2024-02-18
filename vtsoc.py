ccb3b6e0623965b3bfbc446ba815c0d22d56400f
Karl Besser <kb1717@princeton.edu>
2024-02-18 18:42:11 -0500

    Update Vehicular Technology script for new website and add TVT cfp

--- vtsoc.py
+++ vtsoc.py
@@ -7,33 +7,44 @@ from bs4 import BeautifulSoup
 
 from constants import COL_JOURNAL, COL_DEADLINE, COL_PUB_DATE, COL_TOPIC
 
-URL = "https://vtsociety.org/publication/ieee-ojvt/special-issues"
 URL_SOC = "https://vtsociety.org"
+JOURNALS = {
+        "OJVT": "https://vtsociety.org/publication/ieee-ojvt/special-issues",
+        "TVT": "https://vtsociety.org/publication/transactions-vehicular-technology/call-papers",
+        }
 
-RE_DATE = r"Manuscript submission: (.+? \d{1,2}, \d{4})(?:.*)Final publication: (.+)"
+#RE_DATE = r"Manuscript submission: (.+? \d{1,2}, \d{4})(?:.*)Final publication: (.+)"
+RE_DATE = r"(?:.*)Deadline:(\d{1,2} \w+ \d{4})(?:.*)"
 
 
 def get_all_cfp():
-    resp = requests.get(URL)
+    data = []
+    for journal, url in JOURNALS.items():
+        data.append(parse_journal_cfp(url, journal))
+    data = pd.concat(data, ignore_index=True)
+    # data = translate_data_formats(data)
+    return data
+
+def parse_journal_cfp(url: str, journal: str):
+    resp = requests.get(url)
     soup = BeautifulSoup(resp.text, "html.parser")
-    posts = soup("table")[0].find_all("tr")
-    journal = "OJVT"
+    posts = soup("main")[0].find_all("article")
     rows = []
     for post in posts:
-        cells = post.find_all("td")
-        topic = unicodedata.normalize("NFKD", cells[0].text)
+        topic_cell = post.find("h3").find("a")
+        topic = unicodedata.normalize("NFKD", topic_cell.get_text(strip=True))
         try:
-            url_cfp = f"{URL_SOC}{cells[1].find('a')['href']}"
+            url_cfp = f"{topic_cell['href']}"
             topic = f'<a href="{url_cfp}">{topic}</a>'
         except:
             url_cfp = ""
-        date_string = cells[2].text
-        date_string = unicodedata.normalize("NFKD", date_string)
+        pub_date = "Unknown"
         try:
-            due_date, pub_date = re.match(RE_DATE, date_string).groups()
+            _match = re.match(RE_DATE, post.get_text(strip=True))
+            due_date = _match.groups()[0]
         except:
             continue
-        journal = f'<a href="{URL}">{journal}</a>'
+        journal = f'<a href="{url}">{journal}</a>'
         rows.append([topic, due_date, pub_date, journal])
     if not rows:
         raise ValueError("No entries found")
