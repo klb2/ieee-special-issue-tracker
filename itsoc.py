5d2ebbd2e9566a263b74cb1a76d4c4f1ac8102f3
Karl-Ludwig Besser <karl-ludwig.besser@liu.se>
2025-07-21 12:18:48 +0200

    Update parsing of IT Soc calls

--- itsoc.py
+++ itsoc.py
@@ -1,4 +1,5 @@
 import re
+import logging
 
 import pandas as pd
 import requests
@@ -6,39 +7,43 @@ from bs4 import BeautifulSoup
 
 from constants import COL_JOURNAL, COL_DEADLINE, COL_PUB_DATE, COL_TOPIC
 
+LOGGER = logging.getLogger("itsoc")
+
 URL = "https://www.itsoc.org/jsait/calls-for-papers"
 URL_SOC = "https://www.itsoc.org"
 
 RE_ENTRY_TITLE = r"(?:CFP: )?(.+?)\s?(?:(\(Closed\)|\(Currently Open\))|$)"
-RE_DATE = r".+ ((?:\d{1,2} )?\w+ (?:\d{1,2}, )?\d{4})"
+RE_DATE = r"Deadline:\s+((?:\d{1,2} )?\w+ (?:\d{1,2}, )?\d{4})"
 
 
 def get_all_cfp():
     resp = requests.get(URL)
+    LOGGER.debug(f"Response code: {resp.status_code}")
     soup = BeautifulSoup(resp.text, "html.parser")
-    entries = soup.find_all("div", "content--teaser--card")
+    # LOGGER.debug(soup)
+    entries = soup.find_all("div", {"class": "call-for-papers"})
+    LOGGER.debug(f"Found a total of {len(entries):d} entries")
     rows = []
     journal = f'<a href="{URL}">JSAIT</a>'
     for entry in entries:
-        _strings = list(entry.stripped_strings)
-        if len(_strings) < 4:
+        if "call-for-papers--closed" in entry["class"]:
             continue
-        topic = re.match(RE_ENTRY_TITLE, str(_strings[0]))[1]
+        title_div = entry.find("div", {"class": "call-for-papers__title"})
+        _topic = title_div.get_text(strip=True)
+        topic = re.match(RE_ENTRY_TITLE, _topic)[1]
+        url_cfp = title_div.find("a")["href"]
+        url_cfp = f"{URL_SOC}{url_cfp}"
+        topic = f'<a href="{url_cfp}">{topic}</a>'
+
         try:
-            cfp_file_div = entry.find_all("div", "file")[0]
-            url_cfp = cfp_file_div.find("a")["href"]
-            url_cfp = f"{URL_SOC}{url_cfp}"
-            topic = f'<a href="{url_cfp}">{topic}</a>'
-        except:
-            url_cfp = ""
-        # description = _strings[1]
-        try:
-            due_date = re.match(RE_DATE, str(_strings[-2]))[1]
+            date_div = entry.find("div", {"class": "call-for-papers__date"})
+            _date = date_div.get_text(strip=True)
+            due_date = re.match(RE_DATE, _date)[1]
         except:
             continue
         pub_date = "Unknown"
         rows.append([topic, due_date, pub_date, journal])
-    #if not rows:
+    # if not rows:
     #    raise ValueError("No entries found")
     data = pd.DataFrame(
         data=rows, columns=[COL_TOPIC, COL_DEADLINE, COL_PUB_DATE, COL_JOURNAL]
@@ -47,5 +52,9 @@ def get_all_cfp():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s - [%(levelname)8s] %(name)s: %(message)s",
+        level=logging.DEBUG,
+    )
     data = get_all_cfp()
     print(data)
